d35a1500318e8ee9e2072662c22eb1c6fa8046cc
Nwankwo Onyedika Benson <nwankwobenson29@gmail.com>
2024-08-07 11:44:11 +0100

    Created a test app

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,24 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+let people = [
+  {name:'yoshi',age:35,id:1, beltColor:'black'},
+  {name:'lugi',age:40,id:2, beltColor:'red'},
+  {name:'mario',age:45,id:3, beltColor:'blue'}
+]
+
+const handleClick = (id) =>{
+  people = people.filter((person) => person.id != id)
+}
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  {#each people as person(person.id)}
+     <div>
+      <h3>{person.name} is a {person.beltColor} belt</h3>
+      <button on:click={()=> {handleClick(person.id)}}>delete</button>
+     </div>
+  {/each}
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
